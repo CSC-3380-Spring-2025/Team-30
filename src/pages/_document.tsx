15c2dbc578c825d900ac4b9b53810fc66a4b1ef9
iwasella <145622813+iwasella@users.noreply.github.com>
2025-03-15 01:46:03 -0500

    Made class-based approach for server-side data fetching

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from "next/document";
-
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
+import Document, { Html, Head, Main, NextScript } from "next/document";
+export default class CustomDeocument extends Document {
+  render() {
+    return (<Html lang="en">
+      <Head></Head>
       <body className="antialiased">
         <Main />
-        <NextScript />
       </body>
+
+      <NextScript />
+
     </Html>
-  );
+    )
+  }
 }
+
