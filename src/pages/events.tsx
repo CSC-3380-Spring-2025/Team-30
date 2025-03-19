cd0c5e7b1c36547049fa838e97ed70829030f162
beastgamer69420 <achatt5@lsu.edu>
2025-03-18 23:48:24 -0500

    small bug fixes

--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -1,6 +1,6 @@
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
-// import timeGridPlugin from "@fullcalendar/timegrid"; I don't think we'll need this?
+import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 // run npm install @fullcalendar/react @fullcalendar/daygrid @fullcalendar/interaction
 
@@ -8,29 +8,26 @@ function Events() {
   return (
     <>
       <FullCalendar
-        plugins={[dayGridPlugin, interactionPlugin]}
+        plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
         initialView="dayGridMonth"
         headerToolbar = {{
-            start: "prev, next", // previous and next buttons
-            center: "title", // believe it uses month year for title? 
-            end: "timeGridDay, timeGridWeek, dayGridMonth" // day, week, month buttons
+            start: "prev,next", // previous and next buttons
+            center: "title", // month year title
+            end: "timeGridDay,timeGridWeek,dayGridMonth" // day, week, month buttons
         }}
-        //style={{
-        //  width: "500000px",
-        //  height: "500px"
-       // }} // don't think fullcalendar supports style?
       />
-      <button style = {{
-        // using inline, not sure if there's a plan for a button componenet
+      <button
+      style = {{
         backgroundColor: "black",
         color: "white",
         width: "250px",
         height: "50px",
         marginTop: "30px"
       }}
-      >Create Event</button>
+      >
+        Create Event
+      </button>
     </>
-    // this should work..? i don't see why not
   );
 }
 export default Events;
