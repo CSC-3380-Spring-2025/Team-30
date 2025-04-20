5597c6b2b6bc64fb468a337e24e1d788a4525b01
Andrew Dang <63373017+theavgandrew@users.noreply.github.com>
2025-04-19 21:01:57 -0500

    fixed all lint errors in cybersecuritynews, topheader, bottomheader, emblacarousel, and hamburgermenu

--- src/components/TopHeader/topHeader.tsx
+++ src/components/TopHeader/topHeader.tsx
@@ -1,12 +1,12 @@
-// components/TopHeader.tsx
 import Image from "next/image";
 import HamburgerMenu from "@/components/HamburgerMenu/hamburgerMenu";
 import styles from "./TopHeader.module.css";
+import Link from "next/link"; 
 
 const TopHeader = () => {
   return (
     <div className={styles.topBox}>
-      <a href="/">
+      <Link href="/"> 
         <Image
           src="/ssl-logo.png"
           width={75}
@@ -14,7 +14,7 @@ const TopHeader = () => {
           className={styles.logo}
           alt="Logo"
         />
-      </a>
+      </Link>
       <HamburgerMenu />
     </div>
   );
