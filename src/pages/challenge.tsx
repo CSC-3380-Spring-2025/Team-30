50d34c175194d898d15937cf5c68784aef2a92e5
Cam Shortt <c@cshortt.com>
2025-04-28 17:28:18 -0500

    add semi-functional vm frontend

---
+++ src/pages/challenge.tsx
@@ -0,0 +1,35 @@
+import { VT323 } from "next/font/google";
+import { useRouter } from "next/router";
+import styles from "./CTFS.module.css";
+import Script from "next/script"
+import Link from "next/link"
+
+
+const vt323 = VT323({
+    weight: '400',
+    subsets: ['latin'],
+});
+
+export default function Home() {
+    const router = useRouter();
+    const { query } = router;
+
+    const challenge = query.n;
+    const challengeData = require("./challenges.json");
+
+    if (! challengeData[challenge]) {
+	    return (
+		    <p>This CTF does not exist</p>
+	    );
+    }
+
+    if (challengeData[challenge].sys === "vm-manager") {
+        return (
+		<Link src="/vm/vnc.html" />
+        );
+    }
+
+    return (
+	    <p>Something went wrong.</p>
+    );
+}
